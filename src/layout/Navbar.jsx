ea3273bc607c426ce933d9ee5cfac7dc0b2c1308
Codify Technologies Limited <danielidah608@gmail.com>
2026-02-04 09:45:12 +0100

    completed routing logic

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,3 +1,32 @@
+const navLinks = [
+    {href: "#about", label: "About"},
+    {href: "#projects", label: "Projects"},
+    {href:"#experience", label: "Experience"},
+    {href: "#testimonials", label: "Testimonials"},
+]
+
 export const Navbar = () => {
-    return <header></header>
+    return (
+        <header className="fixed top-0 left-0 right-0 bg-transparent py-5">
+            <nav className="container mx-auto px-6 flex items-center justify-between">
+                <a href="#" className="text-xl font-bold tracking-tight">
+                    ID <span>.</span>
+                </a>
+
+                {/* Desktop Nav */}
+                <div>
+                    <div>
+                        {navLinks.map((link, index) => {
+                            return (
+                            <a href={link.href} key={index}>
+                                {link.label}
+                            </a>
+                        )
+                        })}
+
+                    </div>
+                </div>
+            </nav>
+        </header>
+    )
 }
